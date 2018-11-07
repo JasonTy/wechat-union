86a70e4859614a8ce7ba49da14171c04651a946a
JasonTy <1026443062@qq.com>
2018-11-07 13:32:02 +0800

    update

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"crypto/cipher"
 	"crypto/aes"
 	"encoding/base64"
+	"github.com/json-iterator/go"
 	
 )
 //Watermark
@@ -24,6 +24,7 @@ type User struct {
 	Province string `json:"province"`
 	Country string `json:"country"`
 	AvatarUrl string `json:"avatarUrl"`
+	UnionId string `json:"unionId"`
 	Watermark Watermark `json:"watermark"`
 }
 
@@ -49,9 +50,10 @@ func AesDecrypt(encrypted, key []byte, iv []byte) ([]byte, error) {
 }
 
 func main () {
-	var sessionKey = ""
-	var encryptedData = ""
-	var iv = ""
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	var sessionKey = "tiihtNczf5v6AKRyjwEUhQ=="
+	var encryptedData = "CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=="
+	var iv = "r7BXXKkLb8qrSNn05n0qiA=="
 	decoded, _ := base64.StdEncoding.DecodeString(sessionKey)
 	encrypteddata, _ := base64.StdEncoding.DecodeString(encryptedData)
 	ivdata, _ := base64.StdEncoding.DecodeString(iv)
@@ -59,11 +61,11 @@ func main () {
 	if err != nil {
 		panic(err)
 	}
-	var jsonUsers = []byte(string(origData))
+
 	users := User{}
-	errs := json.Unmarshal(jsonUsers, &users)
+	errs := json.Unmarshal(origData, &users)
 	if errs != nil {
-		fmt.Println(errs)
+		//fmt.Println(errs)
 	}
 	fmt.Println(users)
 }
