3c53cf4159573c2b6f7472f9ee25fc1e74e5915b
JasonTy <1026443062@qq.com>
2018-11-11 17:33:06 +0800

    update spew

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"crypto/cipher"
 	"crypto/aes"
+	"crypto/cipher"
 	"encoding/base64"
-	"github.com/json-iterator/go"
-	
+	"encoding/json"
+	"github.com/davecgh/go-spew/spew"
+	"strings"
 )
 //Watermark
 type Watermark struct {
@@ -50,13 +50,12 @@ func AesDecrypt(encrypted, key []byte, iv []byte) ([]byte, error) {
 }
 
 func main () {
-	var json = jsoniter.ConfigCompatibleWithStandardLibrary
 	var sessionKey = "tiihtNczf5v6AKRyjwEUhQ=="
 	var encryptedData = "CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=="
 	var iv = "r7BXXKkLb8qrSNn05n0qiA=="
 	defer func () {
 		if e := recover(); e != nil {
-			fmt.Println(e)
+			spew.Dump(e)
 		}
 	}()
 	decoded, _ := base64.StdEncoding.DecodeString(sessionKey)
@@ -67,12 +66,13 @@ func main () {
 		panic(err)
 	}
 
+	newStr := strings.Replace(string(origData), "\a", "", -1)
 	users := User{}
-	errs := json.Unmarshal(origData, &users)
+	errs := json.Unmarshal([]byte(newStr), &users)
 	if errs != nil {
 		panic(errs)
 	}
-	fmt.Println(users)
+	spew.Println(users)
 }
 
 
